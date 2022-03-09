1626e1ed132332fe5b30547174a46d3b8b364866
Sahib pratap singh <59799717+sahib-pratap-singh@users.noreply.github.com>
2022-03-09 14:56:59 +0530

    Time: 35 ms (58.65%), Space: 9 MB (88.79%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void create(int s, int n, int k, vector<vector<int>>& result, vector<int> &cur){
+        if(cur.size()==k){
+            result.push_back(cur);
+            return;
+        }
+        else{
+            for(int i=s;i<=n;i++){
+                cur.push_back(i);
+                create(i+1,n,k,result,cur);
+                cur.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> cur;
+        create(1,n,k,result, cur);
+        return result;
+    }
+};
