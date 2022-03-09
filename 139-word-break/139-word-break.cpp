127d5c11d9064b66b25be12638a2402eb2560ba5
Sahib pratap singh <59799717+sahib-pratap-singh@users.noreply.github.com>
2022-03-09 10:25:32 +0530

    Time: 38 ms (19.76%), Space: 13.1 MB (47.09%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& dict) {
+        if(dict.size()==0) return false;
+        
+        vector<bool> dp(s.size()+1,false);
+        dp[0]=true;
+        
+        for(int i=1;i<=s.size();i++)
+        {
+            for(int j=i-1;j>=0;j--)
+            {
+                if(dp[j])
+                {
+                    string word = s.substr(j,i-j);
+                    for(auto x:dict){
+                        if(word==x){
+                            dp[i]=true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
