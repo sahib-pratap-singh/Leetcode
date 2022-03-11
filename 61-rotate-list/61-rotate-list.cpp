f03f49cc092ddb4497bb44d5a0e7ed926172c578
Sahib pratap singh <59799717+sahib-pratap-singh@users.noreply.github.com>
2022-03-11 09:01:30 +0530

    Time: 14 ms (34.35%), Space: 11.8 MB (55.87%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -10,26 +10,50 @@
  */
 class Solution {
 public:
-    ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL or head->next==NULL){
-            return head;
+    //Reverse a linked list
+    ListNode* reverseList(ListNode* root) {
+        ListNode* prev = NULL;
+        ListNode* curr = root;
+        while (curr != NULL) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
         }
-        int length = 0;
+        return prev;
+    }
+    
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL)
+            return 0;
         ListNode* newhead = head;
+        int length=0;
         while(newhead!=NULL){
             length++;
-            if(newhead->next==NULL){
-                newhead->next=head;
-                break;
-            }
             newhead=newhead->next;
         }
-        k = length - k%length;
-        for(int i=0;i<k;i++){
-            newhead=newhead->next;
+        k = k%length;
+        if(k==0)
+            return head;
+        head=reverseList(head);
+        ListNode* kthnode=head;
+        while(k>1 && kthnode){
+            kthnode=kthnode->next;
+            k--;
         }
-        head=newhead->next;
-        newhead->next=NULL;
-        return head;
+        //ListNode* secondhalf=NULL;
+        if(kthnode==NULL || kthnode->next==NULL)
+            return head;
+        
+        ListNode* secondhalf=reverseList(kthnode->next);
+        kthnode->next=NULL;
+        
+        ListNode* firsthalfend=reverseList(head);
+        ListNode* result=firsthalfend;
+        while(firsthalfend && firsthalfend->next){
+            firsthalfend=firsthalfend->next;
+        }
+        firsthalfend->next=secondhalf;
+        return result;
     }
 };
