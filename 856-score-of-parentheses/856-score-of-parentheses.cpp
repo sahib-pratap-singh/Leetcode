7f2221b7b4117490873c2fd8be4d84c556095cc9
Sahib pratap singh <59799717+sahib-pratap-singh@users.noreply.github.com>
2022-03-17 09:51:09 +0530

    Time: 0 ms (100.00%), Space: 6 MB (83.25%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-        int cur=0;
-        stack<int> sp;
+        int ans=0;
+        int cnt=0;
         for(int i=0;i<s.length();i++){
-            if(s[i]=='('){
-                sp.push(cur);
-                cur=0;
-            }
+            if(s[i]=='(')
+                cnt++;
             else{
-                cur=sp.top()+max(2*cur,1);
-                sp.pop();
+                cnt--;
+                if(s[i-1]=='(')
+                    ans+=1<<cnt;
             }
         }
-        return cur;
+        return ans;
     }
 };
