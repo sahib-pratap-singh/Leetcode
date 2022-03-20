79937b9c158fc32c7c24433ce6bcc5f845acf5e4
Sahib pratap singh <59799717+sahib-pratap-singh@users.noreply.github.com>
2022-03-20 06:27:40 +0530

    Time: 159 ms (71.36%), Space: 111.5 MB (94.45%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -2,27 +2,32 @@ class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
         int n=tops.size();
-        //Count the occurence of each number in tops row
-        vector<int> countA(7,0);
-        //Count the occurence of each number in bottom row
-        vector<int> countB(7,0);
-        //Count the occurence of numbers which are same in tops and bottom at same index
-        vector<int> same(7,0);
-        
-        //Iterate from 0 to n
+        int countA=0;
+        int countB=0;
         for(int i=0;i<n;i++){
-            //Increment the count of tops[i]
-            countA[tops[i]]++;
-            //Increment the count of bottoms[i]
-            countB[bottoms[i]]++;
-            //Increment the count if both are same
-            if(tops[i]==bottoms[i])
-                same[tops[i]]++;
+            if(tops[i]==tops[0] || bottoms[i]==tops[0]){
+                if(tops[i]!=tops[0])
+                    countA++;
+                if(bottoms[i]!=tops[0])
+                    countB++;
+                if(i==n-1)
+                    return min(countA,countB);
+            }else{
+                break;
+            }
         }
-        
-        for(int i=0;i<7;i++){
-            if((countA[i]+countB[i]-same[i])==n){
-                return n-max(countA[i],countB[i]);
+        countA=0;
+        countB=0;
+        for(int i=0;i<n;i++){
+            if(tops[i]==bottoms[0] || bottoms[i]==bottoms[0]){
+                if(tops[i]!=bottoms[0])
+                    countA++;
+                if(bottoms[i]!=bottoms[0])
+                    countB++;
+                if(i==n-1)
+                    return min(countA,countB);
+            }else{
+                break;
             }
         }
         return -1;
